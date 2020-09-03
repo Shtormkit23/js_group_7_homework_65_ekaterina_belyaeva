02b691b4de7909a1f2a03dd5366e6b00fd222280
Belyaeva Ekaterina <katrinamalinashtorm@gmail.com>
2020-09-03 18:06:17 +0600

    Добавлен компонент Pages.js

---
+++ src/Pages.js
@@ -0,0 +1,8 @@
+export const PAGES = [
+    {name: 'Home', id: 'home'},
+    {name: 'About', id: 'about'},
+    {name: 'Contacts', id: 'contacts'},
+    {name: 'Info', id: 'info'},
+    {name: 'Menu', id: 'menu'},
+    {name: 'Blog', id: 'blog'}
+];
